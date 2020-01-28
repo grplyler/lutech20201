e190f3ab593bc679f322f8241e8bf1ed241e7f37
Ryan Plyler <g.r.plyler@gmail.com>
2020-01-28 16:28:29 -0500

    made starter template

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, escape, request, render_template
 
+
+# Create Flask App
 app = Flask(__name__)
 
 
@@ -14,30 +16,6 @@ def index():
 def calc():
     return render_template('calc.html')
 
-# Calculator Reults Page
-@app.route('/calc', methods=['POST'])
-def calc_results():
-
-    # Perform Loan Calculation
-    years = int(request.form['years'])
-    n = years * 12
-    interest = float(request.form['interest'])
-    i = interest / 12
-    PVa = int(request.form['present_value'])
-
-
-    monthly_payment = (i * PVa) / (1 - pow(1+i, -n))
-    monthly_payment = "{:.2f}".format(monthly_payment)
-
-    # Results
-    results = {
-        PVa: PVa,
-        interest: interest,
-        years: years,
-        monthly_payment: monthly_payment
-    }
-
-    return render_template('calc_results.html', monthly_payment=monthly_payment)
 
 if __name__ == "__main__":
     app.run(debug=True)
